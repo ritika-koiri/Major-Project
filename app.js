ee20e8990730cdeb8f353277647f059acc75a3f3
Ritika koiri <koiriritika738@gmail.com>
2026-05-11 01:18:30 +0530

    Use new app.ejs layout to fix corrupted file

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
-app.engine('ejs', ejsMate);
+app.set('layout', 'layouts/app');
 app.use(express.static(path.join(__dirname, "/public")));
 
 
